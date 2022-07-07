9c087d44396b150baead32a52f4a8d51b9068110
518H0090 <nhoxhieuro5@gmail.com>
2022-07-07 20:46:42 +0700

    Operator

--- Operator.dart
+++ Operator.dart
@@ -7,4 +7,20 @@ void main(List<String> arguments) {
 
   // chia lấy dư
   print(5 % 2);
+
+  // Toán tử 3 ngôi
+
+  var a = 5;
+  var b = 10;
+
+  print(a < b ? "True" : "False");
+
+  var aNumber = 5;
+  var bNumber;
+
+  print(aNumber ?? bNumber);
+
+  // List
+  List temp = [1, 12, 23];
+  print(temp[2]);
 }
