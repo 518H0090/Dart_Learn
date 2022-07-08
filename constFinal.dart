8c2adf54591a04b47b24ab2a1f5d7ed78bf0734a
518H0090 <nhoxhieuro5@gmail.com>
2022-07-08 12:17:56 +0700

    Final and Const

---
+++ constFinal.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // const dùng cho số phải gấn giá trị
+  const double pi = 3.14562;
+
+  print(pi);
+
+  // final dùng cho text gán giá trị 1 lần duy nhất không được thay đổi
+  final String valueString = "Key Member";
+
+  print(valueString);
+}
